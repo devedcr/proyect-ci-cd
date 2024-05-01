27358666cf98eb62f435853f011d2eb24c437a7b
eduardo <jorge.ecrg@gmail.com>
2024-05-01 11:40:47 -0400

    Add Reponsive Web in the table notes

--- public/assets/js/html/main_html.js
+++ public/assets/js/html/main_html.js
@@ -3,8 +3,8 @@ export function htmlTableItem(id,name,date_created,date_end) {
     <tr class="table_row text-center py-2 border-b" style="padding: 10px;">
         <td class="p-2">${id}</td>
         <td class="p-2">${name}</td>
-        <td class="p-2">${date_created}</td>
-        <td class="p-2">${date_end}</td>
+        <td class="p-2 hidden sm:table-cell">${date_created}</td>
+        <td class="p-2 hidden sm:table-cell">${date_end}</td>
         <td class="p-2">
         <button class="border px-3 py-[2px] rounded shadow btn__trash">
             <ion-icon name="trash"></ion-icon>
